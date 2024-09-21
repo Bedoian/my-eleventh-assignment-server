4ef000883fad66aa6ef9639a3269e127c0fadf21
Rafioul Hasan <rafioulhasan2@gmail.com>
2024-09-21 12:18:12 +0600

    cookie clear added when user will logged out

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const jwt=require('jsonwebtoken')
-const cookieParser=require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const cors = require('cors');
@@ -41,23 +41,35 @@ async function run() {
 
 
     const itemCollection = client.db("FoodDB").collection("foods")
-    const purchaseCollection=client.db('FoodDB').collection('purchase')
-    const reviewCollection=client.db('FoodDB').collection('review')
+    const purchaseCollection = client.db('FoodDB').collection('purchase')
+    const reviewCollection = client.db('FoodDB').collection('review')
 
     // generate jwt
-    app.post('/jwt',async(req,res)=>{
-      const user=req.body;
-      console.log(user);
-      const token=jwt.sign(user,process.env.SECRET_TOKEN_KEY,{
-        expiresIn:'10h'
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.SECRET_TOKEN_KEY, {
+        expiresIn: '10h'
       })
       res
-      .cookie('token',token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        sameSite:process.env.NODE_ENV==='production'?'none':'strict'
-      })
-      .send({success:true})
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        })
+        .send({ success: true })
+    })
+
+    // clear jwt token
+    app.get('/logout', (req, res) => {
+      res
+        .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0,
+        })
+        .send({ success: true })
     })
 
     // post item 
@@ -87,70 +99,72 @@ async function run() {
 
     // post oparation of purchase collection
 
-    app.post('/myPurchase',async(req,res)=>{
-      const newPurchase=req.body
-      const result=await purchaseCollection.insertOne(newPurchase)
+    app.post('/myPurchase', async (req, res) => {
+      const newPurchase = req.body
+      const result = await purchaseCollection.insertOne(newPurchase)
       res.send(result)
     })
 
     // for email query
-    app.get('/myAddedItem',async(req,res)=>{
-      const result=await itemCollection.find().toArray()
+    app.get('/myAddedItem', async (req, res) => {
+      const result = await itemCollection.find().toArray()
       res.send(result)
     })
     // single email query item 
-    app.get('/myAddedItem/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await itemCollection.findOne(query)
+    app.get('/myAddedItem/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await itemCollection.findOne(query)
       res.send(result)
     })
     // my added item
-    app.get('/myAdded/:email',async(req,res)=>{
-      const email=req.params.email
-      const query={'buyer.email':email}
-      const result=await itemCollection.find(query).toArray()
+    app.get('/myAdded/:email', async (req, res) => {
+      const token=req.cookies.token
+      console.log('the token is', token);
+      const email = req.params.email
+      const query = { 'buyer.email': email }
+      const result = await itemCollection.find(query).toArray()
       res.send(result)
     })
     //update added items
-    app.put('/myAddedItem/:id',async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const options={upsert:true};
-      const itemData=req.body;
-      const updatedDoc={
-        $set:{
+    app.put('/myAddedItem/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const itemData = req.body;
+      const updatedDoc = {
+        $set: {
           ...itemData
         }
       }
-      const result=await itemCollection.updateOne(query,updatedDoc,options)
+      const result = await itemCollection.updateOne(query, updatedDoc, options)
       res.send(result)
     })
 
     // get purchase item depending on the email
-    app.get('/purchaseItem/:email',async(req,res)=>{
-      const email=req.params.email;
-      const query={'buyer.email':email}
-      const result=await purchaseCollection.find(query).toArray()
+    app.get('/purchaseItem/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { 'buyer.email': email }
+      const result = await purchaseCollection.find(query).toArray()
       res.send(result)
     })
 
     // delete my purchase data
-    app.delete('/purchase/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id:new ObjectId(id)}
-      const result=await purchaseCollection.deleteOne(query)
+    app.delete('/purchase/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await purchaseCollection.deleteOne(query)
       res.send(result)
     })
     // post review
-    app.post('/review',async(req,res)=>{
-      const newReview=req.body;
-      const result=await reviewCollection.insertOne(newReview)
+    app.post('/review', async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewCollection.insertOne(newReview)
       res.send(result)
     })
     // get review
-    app.get('/review',async(req,res)=>{
-      const result=await reviewCollection.find().toArray()
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray()
       res.send(result)
     })
 
