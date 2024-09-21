9ab31ba44e5856ed5435dd91f087687c521dc248
Rafioul Hasan <rafioulhasan2@gmail.com>
2024-09-21 10:51:49 +0600

    jwt initial enviroment setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
+const jwt=require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const cors = require('cors');
